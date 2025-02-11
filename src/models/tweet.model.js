04def9c4c9ce79e4741410d2241edc3293704791
Dheeraj Kumar <dheerajmahourji@gmail.com>
2025-02-11 14:16:54 +0530

    add comment, like, and playlist models with mongoose schemas

---
+++ src/models/tweet.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const tweetSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Tweet = mongoose.model("Tweet", tweetSchema);
